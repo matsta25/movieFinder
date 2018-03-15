19a919799e001d29c11fda2e51b3ae512d42742b
matsta25 <matim2112@gmail.com>
2018-03-16 00:45:59 +0100

    changed http to https

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ $(document).ready(() => {
 });
 
 function getMovies(searchText) {
-    axios.get('http://www.omdbapi.com/?apikey=a100a7e&s=' + searchText)
+    axios.get('https://www.omdbapi.com/?apikey=a100a7e&s=' + searchText)
         .then((response) => {
             let movies = response.data.Search;
             let output = '';
@@ -40,7 +40,7 @@ function movieSelected(id) {
 function getMovie() {
     let movieId = sessionStorage.getItem('movieId');
 
-    axios.get('http://www.omdbapi.com/?apikey=a100a7e&i=' + movieId)
+    axios.get('https://www.omdbapi.com/?apikey=a100a7e&i=' + movieId)
         .then((response) => {
             console.log(response);
             let movie = response.data;
@@ -69,7 +69,7 @@ function getMovie() {
                         <h3>Plot</h3>
                         ${movie.Plot}
                         <hr>
-                        <a href="http://imdb.com/title/${movie.imdbID}" target="_blank" class="btn btn-primary">View IMDB</a>
+                        <a href="https://imdb.com/title/${movie.imdbID}" target="_blank" class="btn btn-primary">View IMDB</a>
                         <a href="index.html" class="btn btn-dark">Go Back To Search</a>
                     </div>
                 </div>
